6ffb612cafaa869f1259ff8e176211e4b3bfedc8
sagarcusat <arayanamit123@gmail.com>
2015-02-08 03:20:31 +0530

    Amit kumar

--- assignments/assignment1/solutions1-java/CupsAndSaucers.java
+++ assignments/assignment1/solutions1-java/CupsAndSaucers.java
@@ -1,27 +1,56 @@
-public class CupsAndSaucers {
-	public static void main(String args[]) {
-		int T[] = { 15, 20, 20, 22, 30, 89 };
-		int S[] = { 10, 19, 26, 30, 35, 90, 95 };
-		System.out.println("Max no of pairings are: "
-				+ getMaxNumberOfPairs(T, S, 6, 7));
+public class CupSaucer {
+	public static void main(String arg[]){
+		int []c={ 15, 20, 20, 22, 30, 89 };
+		int s[] = { 10, 19, 26};
+		int pair=findMaxPair(c, s);
+		System.out.println("pairs are "+pair);
 
 	}
+	
+	//useses half exclusions
+	public static int findMaxPair(int [] c,int [] s){
+		int pair=0;
 
-	static int getMaxNumberOfPairs(int T[], int S[], int noOfCups,
-			int noOfSaucers) {
-		int i = 0;
-		int j = 0;
-		int maxNoOfPairings = 0;
-		while (i < noOfCups && j < noOfSaucers) {
-			if (S[j] >= T[i]) {
-				System.out.println(T[i] + " " + S[j]);
-				i++;
-				j++;
-				maxNoOfPairings++;
-			} else
-				j++;
-
+		for(int i=c.length-1,j=s.length-1;(i>=0&&j>=0);){
+			//			for(int j=s.length-1;c[i]<=s[j];j--){
+			if(s[j]<c[0]){
+				return pair;
+			}
+			i=findsmallcup(c, i, s[j]);
+			if(s[j]>=c[i]){
+				System.out.println(" ("+c[i]+", "+s[j]+")");
+				pair++;
+				--i;
+				--j;
+			}
+			//}
 		}
-		return maxNoOfPairings;
+
+		return pair;
 	}
+
+	static int findsmallcup(int []c,int lindex,int s){
+		
+		int sin=0;
+		int lin=lindex;
+		int mid=(lin+sin)/2;
+		while(lin>sin){
+			
+
+			if(c[mid]>s){
+				lin=mid-1;
+				mid=(lin+sin)/2;
+			}
+			if(c[mid]<s){
+			sin=mid+1;
+			mid=(sin+lin)/2;
+			}if(c[mid]==s){
+				return mid;
+			}
+		}
+		return mid;
+
+
+	}
+	
 }
